c57d1b25dcb7080dfea0c16c7dedaf2764eac359
vidurkhanal <vkhanal@caldwell.edu>
2024-01-13 00:23:37 -0500

    chore: introduced DeepInfra to globals

--- src/globals.ts
+++ src/globals.ts
@@ -5,38 +5,49 @@ export const HEADER_KEYS: Record<string, string> = {
   RETRIES: `x-${POWERED_BY}-retry-count`,
   PROVIDER: `x-${POWERED_BY}-provider`,
   TRACE_ID: `x-${POWERED_BY}-trace-id`,
-  CACHE: `x-${POWERED_BY}-cache`
-}
+  CACHE: `x-${POWERED_BY}-cache`,
+};
 
 export const RESPONSE_HEADER_KEYS: Record<string, string> = {
   RETRY_ATTEMPT_COUNT: `x-${POWERED_BY}-retry-attempt-count`,
   LAST_USED_OPTION_INDEX: `x-${POWERED_BY}-last-used-option-index`,
   LAST_USED_OPTION_PARAMS: `x-${POWERED_BY}-last-used-option-params`,
   CACHE_STATUS: `x-${POWERED_BY}-cache-status`,
-  TRACE_ID: `x-${POWERED_BY}-trace-id`
-}
+  TRACE_ID: `x-${POWERED_BY}-trace-id`,
+};
 
 export const RETRY_STATUS_CODES = [429, 500, 502, 503, 504];
 export const MAX_RETRIES = 5;
 
-export const OPEN_AI:string = "openai";
-export const COHERE:string = "cohere";
-export const AZURE_OPEN_AI:string = "azure-openai";
-export const ANTHROPIC:string = "anthropic";
+export const OPEN_AI: string = "openai";
+export const COHERE: string = "cohere";
+export const AZURE_OPEN_AI: string = "azure-openai";
+export const ANTHROPIC: string = "anthropic";
 export const ANYSCALE: string = "anyscale";
 export const PALM: string = "palm";
 export const TOGETHER_AI: string = "together-ai";
 export const GOOGLE: string = "google";
 export const PERPLEXITY_AI: string = "perplexity-ai";
 export const MISTRAL_AI: string = "mistral-ai";
+export const DEEPINFRA: string = "deepinfra";
 
-export const providersWithStreamingSupport = [OPEN_AI, AZURE_OPEN_AI, ANTHROPIC, COHERE];
-export const allowedProxyProviders = [OPEN_AI, COHERE, AZURE_OPEN_AI, ANTHROPIC];
+export const providersWithStreamingSupport = [
+  OPEN_AI,
+  AZURE_OPEN_AI,
+  ANTHROPIC,
+  COHERE,
+];
+export const allowedProxyProviders = [
+  OPEN_AI,
+  COHERE,
+  AZURE_OPEN_AI,
+  ANTHROPIC,
+];
 
-export const PROXY_REQUEST_PATH_PREFIX:string = "/v1/proxy";
+export const PROXY_REQUEST_PATH_PREFIX: string = "/v1/proxy";
 
 export const CONTENT_TYPES = {
   APPLICATION_JSON: "application/json",
   MULTIPART_FORM_DATA: "multipart/form-data",
-  EVENT_STREAM: "text/event-stream"
-}
+  EVENT_STREAM: "text/event-stream",
+};
--- src/middlewares/requestValidator/schema/config.ts
+++ src/middlewares/requestValidator/schema/config.ts
@@ -1,99 +1,93 @@
 import { z } from "zod";
 import {
-    ANTHROPIC,
-    ANYSCALE,
-    AZURE_OPEN_AI,
-    COHERE,
-    GOOGLE,
-    MISTRAL_AI,
-    OPEN_AI,
-    PERPLEXITY_AI,
-    TOGETHER_AI,
+  ANTHROPIC,
+  ANYSCALE,
+  AZURE_OPEN_AI,
+  COHERE,
+  DEEPINFRA,
+  GOOGLE,
+  MISTRAL_AI,
+  OPEN_AI,
+  PERPLEXITY_AI,
+  TOGETHER_AI,
 } from "../../../globals";
 
 export const configSchema: any = z
-    .object({
-        strategy: z
-            .object({
-                mode: z
-                    .string()
-                    .refine(
-                        (value) =>
-                            ["single", "loadbalance", "fallback"].includes(
-                                value
-                            ),
-                        {
-                            message:
-                                "Invalid 'mode' value. Must be one of: single, loadbalance, fallback",
-                        }
-                    ),
-                on_status_codes: z.array(z.number()).optional(),
-            })
-            .optional(),
-        provider: z
-            .string()
-            .refine(
-                (value) =>
-                    [
-                        OPEN_AI,
-                        ANTHROPIC,
-                        AZURE_OPEN_AI,
-                        ANYSCALE,
-                        COHERE,
-                        TOGETHER_AI,
-                        GOOGLE,
-                        PERPLEXITY_AI,
-                        MISTRAL_AI
-                    ].includes(value),
-                {
-                    message:
-                        "Invalid 'provider' value. Must be one of: openai, anthropic, azure-openai, anyscale, cohere",
-                }
-            )
-            .optional(),
-        api_key: z.string().optional(),
-        cache: z
-            .object({
-                mode: z
-                    .string()
-                    .refine((value) => ["simple", "semantic"].includes(value), {
-                        message:
-                            "Invalid 'cache.mode' value. Must be one of: simple, semantic",
-                    }),
-                max_age: z.number().optional(),
-            })
-            .refine((value) => value.mode !== undefined, {
-                message: "'cache.mode' must be defined",
-            })
-            .optional(),
-        retry: z
-            .object({
-                attempts: z.number(),
-                on_status_codes: z.array(z.number()).optional(),
-            })
-            .refine((value) => value.attempts !== undefined, {
-                message: "'retry.attempts' must be defined",
-            })
-            .optional(),
-        weight: z.number().optional(),
+  .object({
+    strategy: z
+      .object({
+        mode: z
+          .string()
+          .refine(
+            (value) => ["single", "loadbalance", "fallback"].includes(value),
+            {
+              message:
+                "Invalid 'mode' value. Must be one of: single, loadbalance, fallback",
+            },
+          ),
         on_status_codes: z.array(z.number()).optional(),
-        targets: z.array(z.lazy(() => configSchema)).optional(),
-    })
-    .refine(
-        (value) => {
-            const hasProviderApiKey =
-                value.provider !== undefined && value.api_key !== undefined;
-            const hasModeTargets =
-                value.strategy !== undefined && value.targets !== undefined;
-            return (
-                hasProviderApiKey ||
-                hasModeTargets ||
-                value.cache ||
-                value.retry
-            );
-        },
+      })
+      .optional(),
+    provider: z
+      .string()
+      .refine(
+        (value) =>
+          [
+            OPEN_AI,
+            ANTHROPIC,
+            AZURE_OPEN_AI,
+            ANYSCALE,
+            COHERE,
+            TOGETHER_AI,
+            GOOGLE,
+            PERPLEXITY_AI,
+            MISTRAL_AI,
+            DEEPINFRA,
+          ].includes(value),
         {
+          message:
+            "Invalid 'provider' value. Must be one of: openai, anthropic, azure-openai, anyscale, cohere",
+        },
+      )
+      .optional(),
+    api_key: z.string().optional(),
+    cache: z
+      .object({
+        mode: z
+          .string()
+          .refine((value) => ["simple", "semantic"].includes(value), {
             message:
-                "Invalid configuration. It must have either 'provider' and 'api_key', or 'strategy' and 'targets', or 'cache', or 'target'",
-        }
-    );
+              "Invalid 'cache.mode' value. Must be one of: simple, semantic",
+          }),
+        max_age: z.number().optional(),
+      })
+      .refine((value) => value.mode !== undefined, {
+        message: "'cache.mode' must be defined",
+      })
+      .optional(),
+    retry: z
+      .object({
+        attempts: z.number(),
+        on_status_codes: z.array(z.number()).optional(),
+      })
+      .refine((value) => value.attempts !== undefined, {
+        message: "'retry.attempts' must be defined",
+      })
+      .optional(),
+    weight: z.number().optional(),
+    on_status_codes: z.array(z.number()).optional(),
+    targets: z.array(z.lazy(() => configSchema)).optional(),
+  })
+  .refine(
+    (value) => {
+      const hasProviderApiKey =
+        value.provider !== undefined && value.api_key !== undefined;
+      const hasModeTargets =
+        value.strategy !== undefined && value.targets !== undefined;
+      return hasProviderApiKey || hasModeTargets || value.cache || value.retry;
+    },
+    {
+      message:
+        "Invalid configuration. It must have either 'provider' and 'api_key', or 'strategy' and 'targets', or 'cache', or 'target'",
+    },
+  );
